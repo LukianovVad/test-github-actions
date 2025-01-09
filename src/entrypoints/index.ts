566fdf80e070ba9444ac77e0e86ff674c55a7c88
Lukianov Vladyslav <v.lukianov@absoluteweb.com>
2025-01-09 20:36:28 +0200

    test commit from dev branch

--- src/entrypoints/index.ts
+++ src/entrypoints/index.ts
@@ -1,6 +1,7 @@
 const main = async () => {
-    console.log('Hello World');
-    console.log('Hello World');
+    console.log('Hello World0');
+    console.log('Hello World1');
+    console.log('Hello World2');
 }
 
 main();
